fc7fa1dc7a18746e75dde403689f56c50e89aad9
Anshul Dewangan <101018041+Anshul-04@users.noreply.github.com>
2023-06-19 23:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+  const input = document.getElementById('rope-input').value;
+  const ropes = input.split(',').map(Number);
+
+  // Create a min-heap priority queue
+  const PriorityQueue = require('./PriorityQueue');
+  const pq = new PriorityQueue();
+
+  // Insert all the rope lengths into the priority queue
+  ropes.forEach((rope) => {
+    pq.insert(rope);
+  });
+
+  let totalCost = 0;
+
+  // Connect the ropes until only one rope remains in the priority queue
+  while (pq.size() > 1) {
+    // Extract the two smallest ropes from the priority queue
+    const rope1 = pq.extractMin();
+    const rope2 = pq.extractMin();
+
+    // Calculate the cost of connecting the ropes
+    const cost = rope1 + rope2;
+
+    // Add the cost to the total cost
+    totalCost += cost;
+
+    // Insert the combined rope length back into the priority queue
+    pq.insert(cost);
+  }
+
+  // Display the minimum cost inside the <div id="result"></div> element
+  const resultDiv = document.getElementById('result');
+  resultDiv.textContent = totalCost.toString();
+}
